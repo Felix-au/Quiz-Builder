ef9fbfa7afedbf8ed2cbfd00a7a39ba92fbe508b
Felix-au <harshit.soni.23cse@bmu.edu.in>
2025-08-08 23:43:45 +0530

    update

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -19,7 +19,7 @@ const manuals = [
   { label: "Admin Manual", href: "/manuals/PrashnaSetu-Application-User-Guide_Admin.pdf" },
   { label: "Instructor Manual", href: "/manuals/PrashnaSetu-Application-User-Guide_Instructor.pdf" },
   { label: "Proctor Manual", href: "/manuals/PrashnaSetu-Application-User-Guide_Proctor.pdf" },
-  { label: "Website Manual", href: "/manuals/PrashnaSetu-Application-User-Guide_Proctor.pdf" },
+  { label: "Website Manual", href: "/manuals/PrashnaSetu-Application-User-Guide_Website.pdf" },
 ];
 
 const downloads = [
